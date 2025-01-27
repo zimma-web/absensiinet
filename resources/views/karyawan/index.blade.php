2d50aea66851fafb9ed1a9a5cf01ab5e69713ced
Zimam <mhmmdzimam@gmail.com>
2025-01-27 12:01:17 +0700

    Episode 23

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -18,43 +18,89 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nik</th>
-                                        <th>Nama</th>
-                                        <th>Jabatan</th>
-                                        <th>No. HP</th>
-                                        <th>Foto</th>
-                                        <th>Departement</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($karyawan as $d)
-                                    @php
-                                        $path = Storage::url('uploads/foto/'.$d->foto);
-                                    @endphp
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $d->nik }}</td>
-                                            <td>{{ $d->nama_lengkap }}</td>
-                                            <td>{{ $d->jabatan }}</td>
-                                            <td>{{ $d->no_hp }}</td>
-                                            <td>
-                                                @if (empty($d->foto))
-                                                <img src="{{ asset('assets/img/nophoto.png') }}" class="avatar" alt="">
-                                                @else
-                                                <img src="{{ url($path) }}" class="avatar" alt="">
-                                                @endif
-                                            </td>
-                                            <td>{{ $d->nama_dept }}</td>
-                                            <td></td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                            <div class="row">
+                                <div class="col-12">
+                                    <form action="/karyawan" method="GET">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <input type="text" name="nama_karyawan" id="nama_karyawan"
+                                                        class="form-control" placeholder="Nama Karyawan" value="{{ Request('nama_karyawan') }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="form-group">
+                                                    <select name="kode_dept" id="kode_dept" class="form-select">
+                                                        <option value="">Departemen</option>
+                                                        @foreach ($departemen as $d)
+                                                        <option {{ Request('kode_dept')==$d->kode_dept ? 'selected' : '' }} value="{{ $d->kode_dept }}">{{ $d->nama_dept }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                            height="24" viewBox="0 0 24 24" fill="none"
+                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                            class="icon icon-tabler icons-tabler-outline icon-tabler-search">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                                                            <path d="M21 21l-6 -6" />
+                                                        </svg>
+                                                        Cari
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-12">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Nik</th>
+                                                <th>Nama</th>
+                                                <th>Jabatan</th>
+                                                <th>No. HP</th>
+                                                <th>Foto</th>
+                                                <th>Departemen</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($karyawan as $d)
+                                                @php
+                                                    $path = Storage::url('uploads/foto/' . $d->foto);
+                                                @endphp
+                                                <tr>
+                                                    <td>{{ $loop->iteration + $karyawan->firstItem() - 1 }}</td>
+                                                    <td>{{ $d->nik }}</td>
+                                                    <td>{{ $d->nama_lengkap }}</td>
+                                                    <td>{{ $d->jabatan }}</td>
+                                                    <td>{{ $d->no_hp }}</td>
+                                                    <td>
+                                                        @if (empty($d->foto))
+                                                            <img src="{{ asset('assets/img/nophoto.png') }}" class="avatar"
+                                                                alt="">
+                                                        @else
+                                                            <img src="{{ url($path) }}" class="avatar" alt="">
+                                                        @endif
+                                                    </td>
+                                                    <td>{{ $d->nama_dept }}</td>
+                                                    <td></td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    {{ $karyawan->links('vendor.pagination.bootstrap-5') }}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
