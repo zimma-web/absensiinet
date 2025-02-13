942511e69606b9fceeca04fea0b66e3911922f29
Zimam <mhmmdzimam@gmail.com>
2025-02-13 20:00:05 +0700

    fix

--- resources/views/auth/loginadmin.blade.php
+++ resources/views/auth/loginadmin.blade.php
@@ -15,11 +15,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Login Admin E-Absensi PT.Inet Global Indo</title>
     <!-- CSS files -->
-    <link href="{{ asset('tabler/dist/css/tabler.min.css?1674944402') }}" rel="stylesheet" />
-    <link href="{{ asset('tabler/dist/css/tabler-flags.min.css?1674944402') }}" rel="stylesheet" />
-    <link href="{{ asset('tabler/dist/css/tabler-payments.min.css?1674944402') }}" rel="stylesheet" />
-    <link href="{{ asset('tabler/dist/css/tabler-vendors.min.css?1674944402') }}" rel="stylesheet" />
-    <link href="{{ asset('tabler/dist/css/demo.min.css?1674944402') }}" rel="stylesheet" />
+<link href="{{ asset('tabler/dist/css/tabler.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('tabler/dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('tabler/dist/css/tabler-payments.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('tabler/dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('tabler/dist/css/demo.min.css') }}" rel="stylesheet" />
     <style>
         @import url('https://rsms.me/inter/inter.css');
 
--- resources/views/presensi/monitoring.blade.php
+++ resources/views/presensi/monitoring.blade.php
@@ -23,7 +23,7 @@
                                 <div class="col-12">
                                     <div class="input-icon mb-3">
                                         <span class="input-icon-addon">
-                                            <!-- Download SVG icon from http://tabler.io/icons/icon/user -->
+<!-- Download SVG icon from https://tabler.io/icons/icon/user -->
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                                 stroke-linecap="round" stroke-linejoin="round"
